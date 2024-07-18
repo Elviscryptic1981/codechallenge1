e71e4b0d8a020db31d2b84bcc3ce799019c2f2b0
Elvis Kimani <162327499+Elviscryptic1981@users.noreply.github.com>
2024-07-18 14:00:43 +0300

    Update speed.js
    Signed-off-by: Elvis Kimani <162327499+Elviscryptic1981@users.noreply.github.com>

--- speed.js
+++ speed.js
@@ -1,19 +1,21 @@
+// Second assignment challenge:Speed Detector
 function speedDetector(speed) {
+    // The following defines the speed limit and the demerit points corresponding to exceeding said speed limit
     const speedLimit = 70;
-    const pointsPerDemerit = 5;
-    const maxPointsBeforeSuspension = 12;
+    const demeritPointsPer5KmOver = 1;
+    const excessSpeed = speed - speedLimit;
 
-    if (speed <= speedLimit) {
-        return "Ok";
+    // The following checks whether the driver's speed is within a suitable range
+    if (excessSpeed <= 0) {
+        return 'Ok';
     } else {
-        const demeritPoints = Math.floor((speed - speedLimit) / pointsPerDemerit);
-        
-        if (demeritPoints >= maxPointsBeforeSuspension) {
-            return "License suspended";
+        // The following calculates the demerit points for exceeding the speed limit
+        const demeritPoints = Math.floor(excessSpeed / 5) * demeritPointsPer5KmOver;
+        // The following checks whether the demerit points qualify the driver for a license suspension
+        if (demeritPoints > 12) {
+            return 'License suspended';
         } else {
-            return "Points: " + demeritPoints;
+            return `Points: ${demeritPoints}`;
         }
     }
 }
-
-let salaryDetails = calculateNetSalary(50000, 10000);
